625a0345e50525179342f607e6a0194ffb907702
ishubhambansal10 <shubham.bansal1@successive.tech>
2021-11-01 13:21:55 +0530

    Minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { ThemeProvider } from '@mui/material/styles';
 import { theme } from './theme';
 // import { ChildrenDemo } from './pages';
-import { Trainee } from './pages/index';
-// import Login from './pages/Login/Login';
+// import { Trainee } from './pages/index';
+import Login from './pages/Login/Login';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
-        <Trainee />
+        <Login />
       </div>
     </ThemeProvider>
   );
